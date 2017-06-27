542d0d6d2e20b52e72fa37cbbb4f6118b3a93169
Pedro Bernardo <Pedro Bernardo>
2017-06-27 12:00:19 +0100

    Add project files.

---
+++ lab1/lab1.Android/Effects/DigitalFontEffect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Widget;
+using Android.Graphics;
+using System.ComponentModel;
+using lab1;
+using lab1.Droid;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(lab1.Droid.DigitalFontEffect), "FontEffect")]
+namespace lab1.Droid
+{
+    public class DigitalFontEffect : PlatformEffect
+    {
+        TextView control;
+        protected override void OnAttached()
+        {
+            try
+            {
+                control = Control as TextView;
+                Typeface font = Typeface.CreateFromAsset(Forms.Context.Assets, "Fonts/" + lab1.DigitalFontEffect.GetFontFileName(Element) + ".ttf");
+                control.Typeface = font;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == lab1.DigitalFontEffect.FontFileNameProperty.PropertyName)
+            {
+                Typeface font = Typeface.CreateFromAsset(Forms.Context.ApplicationContext.Assets, "Fonts/" + lab1.DigitalFontEffect.GetFontFileName(Element) + ".ttf");
+                control.Typeface = font;
+            }
+        }
+    }
+}
